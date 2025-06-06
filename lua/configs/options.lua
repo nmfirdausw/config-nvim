422e5ce4e01a8ce9066fc7d46e11c9f83aa14906
nmfirdausw <nmfirdausw@gmail.com>
2025-06-07 00:41:26 +0800

    feat: add indentation and formatting options
    - Set tab and shift width to 2 spaces
    - Enable expandtab for spaces instead of tabs
    - Configure auto and smart indentation
    - Disable line wrapping for better code readability

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -12,3 +12,12 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.numberwidth = 1
 vim.opt.relativenumber = true
+
+-- Indentation and formatting
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
