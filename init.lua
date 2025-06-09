766545e86f8e85cd2259a0637b9c5d9f25d21634
nmfirdausw <nmfirdausw@gmail.com>
2025-06-09 10:46:41 +0800

    feat: add utils module and which-key plugin
    - Add utils.lua with global utility functions
    - Configure which-key with helix preset and leader key descriptions
    - Load utils globally in init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+_G.Utils = require("configs.utils")
+
 require("configs.options")
 require("configs.diagnostic")
 require("configs.lsp")
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,23 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "helix",
+    win = {
+      no_overlap = true,
+      col = 0,
+      border = vim.g.border,
+    },
+    icons = {
+      mappings = false,
+    },
+    spec = {
+      { "<leader><leader>", desc = "Picker (search)" },
+      { "<leader>s", desc = "Search" },
+      { "<leader>f", desc = "Find" },
+      { "<leader>g", desc = "Git", cond = Utils.gitdir() },
+      { "<leader>t", desc = "Toggle" },
+      { "<leader>u", desc = "UI" },
+    },
+  },
+}
