a9099737b20ffe7835b324f196e0911632b5a015
nmfirdausw <nmfirdausw@gmail.com>
2025-06-09 15:23:35 +0800

    feat: add conform plugin for code formatting

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,32 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "gF",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+    },
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+    format_on_save = { timeout_ms = 2000 },
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2" },
+      },
+    },
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require('conform').formatexpr()"
+  end,
+}
