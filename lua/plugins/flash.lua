13c2305285d6340949536d1a3bdccee91514d4e7
nmfirdausw <nmfirdausw@gmail.com>
2025-06-11 00:29:28 +0800

    feat: re-add flash and which-key plugins
    - Add flash plugin for enhanced navigation with jump functionality
    - Re-add which-key with updated navigation key descriptions
    - Update plugin lock file

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,23 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "s",
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+      mode = { "n", "x", "o" },
+    },
+    {
+      "S",
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+      mode = { "n", "x", "o" },
+    },
+  },
+  opts = {},
+}
