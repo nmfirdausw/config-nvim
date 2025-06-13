3cab276f1031f1aa1310c760b556d90676016051
nmfirdausw <nmfirdausw@gmail.com>
2025-06-13 22:29:26 +0800

    refactor: simplify blink.cmp config and update mason setup
    - Simplify blink.cmp configuration with minimal setup
    - Remove complex custom styling and component logic
    - Update mason.lua with better plugin structure
    - Add null-ls integration for formatting
    - Update lazy-lock.json with new dependencies
    - Add autocmd comments for blink/copilot integration

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,25 +1,64 @@
 return {
-  "WhoIsSethDaniel/mason-tool-installer.nvim",
-  dependencies = {
-    {
-      "williamboman/mason-lspconfig.nvim",
-      cmd = { "LspInstall", "LspUninstall" },
-    },
-    {
-      "williamboman/mason.nvim",
-      cmd = "Mason",
-      build = ":MasonUpdate",
-      opts = {
-        ui = {
-          border = vim.g.border,
-          height = 0.8,
-        },
+  {
+    "mason-org/mason.nvim",
+    cmd = "Mason",
+    build = ":MasonUpdate",
+    opts = {
+      ui = {
+        border = vim.g.border,
+        height = 0.8,
       },
     },
   },
-  lazy = false,
-  opts_extend = { "ensure_installed" },
-  opts = {
-    auto_update = true,
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    lazy = false,
+    opts = {
+      ensure_installed = {
+        -- LSP
+        "lua-language-server",
+        "omnisharp",
+
+        -- Formatters
+        "stylua",
+        "csharpier",
+      },
+      auto_update = true,
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    cmd = { "LspInstall", "LspUninstall" },
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+    opts = {},
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "nvimtools/none-ls.nvim",
+      config = function()
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        require("null-ls").setup({
+          on_attach = function(client, bufnr)
+            if client.supports_method("textDocument/formatting") then
+              vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                  vim.lsp.buf.format({ async = false })
+                end,
+              })
+            end
+          end,
+        })
+      end,
+    },
+    opts = {
+      handlers = {},
+    },
   },
 }
