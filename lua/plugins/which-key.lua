c4d2b665f713cd8d70687af08deeae99f72abbbc
nmfirdausw <nmfirdausw@gmail.com>
2025-07-03 00:23:23 +0800

    feat(which-key): add git-related key descriptions
    - Add conditional git key groups with Utils.gitdir() check
    - Include descriptions for git operations (blame, hunk, navigation)
    - Improve keymap discoverability for git-related functionality

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -13,6 +13,12 @@ return {
     },
     spec = {
       { "<leader>t", desc = "Toggle" },
+      { "<leader>tg", desc = "Git", cond = Utils.gitdir() },
+      { "<leader>g", desc = "Git", cond = Utils.gitdir() },
+      { "<leader>gb", desc = "Blame", cond = Utils.gitdir() },
+      { "<leader>gh", desc = "Hunk", cond = Utils.gitdir() },
+      { "<leader>]", desc = "Next", cond = Utils.gitdir() },
+      { "<leader>[", desc = "Previous", cond = Utils.gitdir() },
     },
   },
 }
