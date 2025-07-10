c56178462c4d7ceb52fe50a9a63ab28c60f6d06c
nmfirdausw <nmfirdausw@gmail.com>
2025-07-10 21:03:18 +0800

    feat: add comprehensive keymaps configuration
    - Add screen centering for search navigation and scrolling
    - Add delete without yanking functionality
    - Add buffer navigation shortcuts
    - Add custom window navigation (Colemak-style bindings)
    - Add window splitting keymaps
    - Add visual mode line movement
    - Add better indenting behavior in visual mode
    - Add improved join lines functionality
    - Add quick config editing shortcut

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,36 @@
+-- Center screen when jumping
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+
+-- Delete without yanking
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d", { desc = "Delete without yanking" })
+
+-- Buffer navigation
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
+
+-- Better window navigation
+vim.keymap.set("n", "<C-m>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-n>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-e>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-i>", "<C-w>l", { desc = "Move to right window" })
+
+-- Splitting & Resizing
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
+
+-- Move lines up/down
+vim.keymap.set("v", "N", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "E", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Better indenting in visual mode
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+-- Better J behavior
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
+
+-- Quick config editing
+vim.keymap.set("n", "<leader>rc", ":e ~/.config/nvim/init.lua<CR>", { desc = "Edit config" })
